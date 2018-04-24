9ee86fc53e2094d1f34e7eb6ab6d01bfc0c01ff0
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-04-24 15:45:37 +0300

    FIR (preliminary): do not build KtLightClasses during supertype resolve

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProviders.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProviders.kt
@@ -7,33 +7,43 @@ package org.jetbrains.kotlin.fir.java
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.asJava.classes.KtLightClass
+import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.java.symbols.JavaClassSymbol
 import org.jetbrains.kotlin.fir.resolve.AbstractFirSymbolProvider
-import org.jetbrains.kotlin.fir.symbols.ConeClassLikeSymbol
+import org.jetbrains.kotlin.fir.service
 import org.jetbrains.kotlin.fir.symbols.ConeSymbol
 import org.jetbrains.kotlin.load.java.structure.JavaClass
 import org.jetbrains.kotlin.load.java.structure.classId
+import org.jetbrains.kotlin.load.java.structure.impl.JavaClassImpl
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.resolve.jvm.KotlinJavaPsiFacade
 
 class JavaSymbolProvider(
     val project: Project,
-    private val searchScope: GlobalSearchScope
+    private val searchScope: GlobalSearchScope,
+    private val session: FirSession
 ) : AbstractFirSymbolProvider() {
 
     override fun getSymbolByFqName(classId: ClassId): ConeSymbol? {
         return classCache.lookupCacheOrCalculate(classId) {
             val facade = KotlinJavaPsiFacade.getInstance(project)
-            val foundClass = facade.findClass(classId, searchScope)
-            foundClass?.let { JavaClassSymbol(this, it) }
+            val foundClass: JavaClass? = facade.findClass(classId, searchScope)
+            if ((foundClass as? JavaClassImpl)?.psi is KtLightClass) {
+                return@lookupCacheOrCalculate null
+            }
+            foundClass?.let { JavaClassSymbol(this, session.service(), it) }
         }
     }
 
     fun getSymbolByJavaClass(javaClass: JavaClass): ConeSymbol? {
         val classId = javaClass.classId ?: error("!")
         return classCache.lookupCacheOrCalculate(classId) {
-            javaClass.let { JavaClassSymbol(this, it) }
+            if ((javaClass as? JavaClassImpl)?.psi is KtLightClass) {
+                return@lookupCacheOrCalculate null
+            }
+            javaClass.let { JavaClassSymbol(this, session.service(), it) }
         }
     }
 
--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/symbols/JavaClassSymbol.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/symbols/JavaClassSymbol.kt
@@ -5,8 +5,10 @@
 
 package org.jetbrains.kotlin.fir.java.symbols
 
+import org.jetbrains.kotlin.asJava.classes.KtLightClass
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.fir.java.JavaSymbolProvider
+import org.jetbrains.kotlin.fir.resolve.FirProvider
 import org.jetbrains.kotlin.fir.symbols.ConeClassSymbol
 import org.jetbrains.kotlin.fir.symbols.ConeTypeParameterSymbol
 import org.jetbrains.kotlin.fir.types.ConeClassErrorType
@@ -14,9 +16,19 @@ import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.impl.ConeClassTypeImpl
 import org.jetbrains.kotlin.load.java.structure.JavaClass
 import org.jetbrains.kotlin.load.java.structure.classId
+import org.jetbrains.kotlin.load.java.structure.impl.JavaClassImpl
+import org.jetbrains.kotlin.load.java.structure.impl.JavaClassifierTypeImpl
 import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.name.tail
 
-class JavaClassSymbol(val symbolProvider: JavaSymbolProvider, javaClass: JavaClass) : ConeClassSymbol {
+class JavaClassSymbol(val symbolProvider: JavaSymbolProvider, val firProvider: FirProvider, javaClass: JavaClass) : ConeClassSymbol {
+
+    init {
+        if ((javaClass as? JavaClassImpl)?.psi is KtLightClass) {
+            throw AssertionError("JavaClassSymbol is built for a light class: ${javaClass.fqName}")
+        }
+    }
+
     override val classId: ClassId = javaClass.classId ?: error("!")
     override val typeParameters: List<ConeTypeParameterSymbol> =
         javaClass.typeParameters.map { JavaTypeParameterSymbol(it.name) }
@@ -28,19 +40,34 @@ class JavaClassSymbol(val symbolProvider: JavaSymbolProvider, javaClass: JavaCla
     }
 
     override val superTypes: List<ConeClassLikeType> by lazy {
-        javaClass.supertypes.map {
-            (it.classifier as? JavaClass)?.let {
-                val symbol = symbolProvider.getSymbolByJavaClass(it)
-                if (symbol != null) {
-                    ConeClassTypeImpl(
-                        symbol as JavaClassSymbol,
-                        emptyArray() // TODO: Fix this?
-                    )
-                } else {
-                    ConeClassErrorType("Unsupported: no (null) symbol for JavaClass supertype: ${it.name}")
-                }
-            } ?: ConeClassErrorType("Unsupported: Non JavaClass superType in JavaClassSymbol") // TODO: Support it
+        val psi = (javaClass as? JavaClassImpl)!!.psi
+        psi.superTypes.mapNotNull {
+            val symbol = if (it is KtLightClass) {
+                val kotlinOrigin = it.kotlinOrigin ?: return@mapNotNull null
+                val fqName = kotlinOrigin.fqName ?: return@mapNotNull null
+                val packageFqName = kotlinOrigin.containingKtFile.packageFqName
+                val classId = ClassId(
+                    packageFqName,
+                    fqName.tail(packageFqName),
+                    false
+                )
+                firProvider.getSymbolByFqName(classId)
+            } else {
+                val type = JavaClassifierTypeImpl(it)
+
+                (type.classifier as? JavaClass)?.let { symbolProvider.getSymbolByJavaClass(it) }
+                        ?: return@mapNotNull ConeClassErrorType("Unsupported: Non JavaClass superType in JavaClassSymbol") // TODO: Support it
+            }
+            if (symbol != null) {
+                ConeClassTypeImpl(
+                    symbol as JavaClassSymbol,
+                    emptyArray() // TODO: Fix this?
+                )
+            } else {
+                ConeClassErrorType("Unsupported: no (null) symbol for JavaClass supertype: ${it.name}")
+            }
         }
+
     }
 }
 
